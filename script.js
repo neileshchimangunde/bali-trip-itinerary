103e58c280bb7893a211f4fa6bc6d3440fd45968
neileshchimangunde <usernc5578@gmail.com>
2025-01-28 23:03:13 +0530

    Update and rename script.js to style.css

--- script.js
+++
@@ -1,8 +0,0 @@
-function markCompleted(taskId) {
-    const taskElement = document.getElementById(taskId);
-    if (taskElement) {
-        taskElement.classList.toggle('completed');
-    } else {
-        console.error(`Task with ID "${taskId}" not found.`);
-    }
-}
