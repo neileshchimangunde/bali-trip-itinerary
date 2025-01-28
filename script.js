72adf435947f3147bf064b5d5bfc04dc87f14870
neileshchimangunde <usernc5578@gmail.com>
2025-01-28 23:44:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function markCompleted(taskId) {
     const taskElement = document.getElementById(taskId);
     if (taskElement) {
-        taskElement.classList.toggle('completed'); // Toggle 'completed' class
+        taskElement.classList.toggle('completed');
     } else {
         console.error(`Task with ID "${taskId}" not found.`);
+    }
+}
+
+function markDayCompleted(dayId) {
+    const dayElement = document.querySelector(`#${dayId}`);
+    if (dayElement) {
+        const tasks = dayElement.querySelectorAll('.task');
+        tasks.forEach(task => task.classList.add('completed'));
+    } else {
+        console.error(`Day with ID "${dayId}" not found.`);
     }
 }
